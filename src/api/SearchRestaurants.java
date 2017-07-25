6334f5405f08f17ee23d6722a27b972a2354928c
JasonW99 <ningweiw@gmail.com>
2017-07-25 02:14:00 -0700

    A utility class to handle rpc related parsing logics.

--- src/api/SearchRestaurants.java
+++ src/api/SearchRestaurants.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -18,36 +19,36 @@ import org.json.JSONObject;
 @WebServlet("/restaurants")
 public class SearchRestaurants extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SearchRestaurants() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SearchRestaurants() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.setContentType("application/json");
-		response.addHeader("Access-Control-Allow-Origin", "*");
-		String username = "";
-		if (request.getParameter("username") != null) {
-		username = request.getParameter("username");
-		}
-		JSONObject obj = new JSONObject();
+		JSONArray array = new JSONArray();
 		try {
-		obj.put("username", username);
+			if (request.getParameterMap().containsKey("user_id")
+					&& request.getParameterMap().containsKey("lat")
+					&& request.getParameterMap().containsKey("lon")) {
+				String userId = request.getParameter("user_id");
+				double lat = Double.parseDouble(request.getParameter("lat"));
+				double lon = Double.parseDouble(request.getParameter("lon"));
+				// return some fake restaurants
+				array.put(new JSONObject().put("name", "Panda Express"));
+				array.put(new JSONObject().put("name", "Hong Kong Express"));
+			}
 		} catch (JSONException e) {
-		e.printStackTrace();
+			e.printStackTrace();
 		}
-		PrintWriter out = response.getWriter();
-		out.print(obj);
-		out.flush();
-		out.close();
+		RpcParser.writeOutput(response, array);
 	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
