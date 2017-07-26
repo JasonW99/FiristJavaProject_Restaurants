b9bf1c79e13664753595f1bccc4eebf50e75796c
JasonW99 <ningweiw@gmail.com>
2017-07-25 18:05:42 -0700

    Mysql algorithm

--- src/api/VisitHistory.java
+++ src/api/VisitHistory.java
@@ -3,12 +3,14 @@ package api;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,27 +33,36 @@ public class VisitHistory extends HttpServlet {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	
+	private static final DBConnection connection = new MySQLDBConnection();
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			JSONArray array = null;
+			if (request.getParameterMap().containsKey("user_id")) {String userId = request.getParameter("user_id");
+			Set<String> visited_business_id = connection.getVisitedRestaurants(userId);
+			array = new JSONArray();
+			for (String id : visited_business_id) {
+				array.put(connection.getRestaurantsById(id, true));
+			}
+			RpcParser.writeOutput(response, array);
+			} else {
+				RpcParser.writeOutput(response, new JSONObject().put("status", "InvalidParameter"));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	private static final DBConnection connection = new MySQLDBConnection();
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
-			// allow access only if session exists
-//			HttpSession session = request.getSession();
-//			if (session.getAttribute("user") == null) {
-//				response.setStatus(403);
-//				return;
-//			}
 			JSONObject input = RpcParser.parseInput(request);
 			if (input.has("user_id") && input.has("visited")) {
 				String userId = (String) input.get("user_id");
@@ -67,6 +78,29 @@ public class VisitHistory extends HttpServlet {
 			} else {
 				RpcParser.writeOutput(response,
 						new JSONObject().put("status", "InvalidParameter"));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			JSONObject input = RpcParser.parseInput(request);
+			if (input.has("user_id") && input.has("visited")) {
+				String userId = (String) input.get("user_id");
+				JSONArray array = (JSONArray) input.get("visited");
+				List<String> visitedRestaurants = new ArrayList<>();
+				for (int i = 0; i < array.length(); i++) {
+					String businessId = (String) array.get(i);
+					visitedRestaurants.add(businessId);
+				}
+				connection.unsetVisitedRestaurants(userId, visitedRestaurants);
+				RpcParser.writeOutput(response, new JSONObject().put("status", "OK"));
+			} else {
+				RpcParser.writeOutput(response, new JSONObject().put("status",
+						"InvalidParameter"));
 			}
 		} catch (JSONException e) {
 			e.printStackTrace();
